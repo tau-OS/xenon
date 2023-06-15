e206164e717610fde2531be7be52cfef2cc02cca
Lleyton Gray <lleyton@fyralabs.com>
2023-06-14 19:03:27 -0700

    Do auth flow on the daemon side

--- server/main.go
+++ server/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/session"
 	jwtware "github.com/gofiber/jwt/v3"
 	"github.com/icza/gog"
 	"github.com/tau-OS/xenon/server/config"
@@ -12,12 +9,7 @@ import (
 	"github.com/tau-OS/xenon/server/user"
 )
 
-var store = session.New()
-
 func main() {
-	if AuthSecret == "" {
-		panic("Oops! `AuthSecret` is not set! Rebuild using this: `go build -ldflags '-X auth.AuthSecret=...' server`")
-	}
 	if err := config.InitializeEnv(); err != nil {
 		panic(err.Error())
 	}
@@ -35,29 +27,6 @@ func main() {
 		Claims:               &user.UserClaims{},
 		KeyRefreshUnknownKID: gog.Ptr(false),
 	}))
-
-	app.Get("/sign-in", func(c *fiber.Ctx) error {
-		if signin(c) {
-			return c.SendStatus(http.StatusInternalServerError)
-		}
-		return nil
-	})
-
-	app.Get("/sign-in-callback", func(c *fiber.Ctx) error {
-		logtoClient := authcallback(c)
-		if logtoClient == nil {
-			return c.SendStatus(500)
-		}
-		user.ChkNew(c)
-		token, err := logtoClient.GetAccessToken("")
-		if err != nil {
-			println("! Can't get access token: " + err.Error())
-			return c.SendStatus(500)
-		}
-		out := "Paste the following into the application: <br>\n"
-		out += token.Token + "." + token.Scope + "." + string(rune(token.ExpiresAt))
-		return c.Send([]byte(out))
-	})
 
 	app.Get("/api/ack", func(c *fiber.Ctx) error {
 		// for acknoledging a client running?
